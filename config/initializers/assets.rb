59e4025633a67f1a7fa7db005b0400e5a65c8774
Anna <anna.brakowska91@gmail.com>
2018-08-27 13:31:27 -0400

    config changes added

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,5 +13,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 #
 
-
-config.assets.precompile += ['session.css']
+Rails.application.config.assets.precompile += %w( session.css )
